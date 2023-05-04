d7e3e64889f4ce140a3e99aa0de6526c931e97b6
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-04 16:44:02 +0300

    Add trips.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TripsComponent } from './trips/trips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule
