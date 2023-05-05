0d6d6799364f0436906a33168d4a1fd118c5cb35
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-05 13:42:28 +0300

    Create fetchTrips function to fetch limited amount data from db when site launched. Add rxjs catchError and throwError.

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 import { Trips } from '../../shared/interfaces'
 
@@ -9,12 +11,39 @@ import { Trips } from '../../shared/interfaces'
   styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
+  private url = 'http://localhost:3000/get'
+
   trips?: Trips[] = [];
+
+  currentPage = 1;
+  totalTrips = 0;
+  tripsPerPage = 10;
 
   constructor (private http: HttpClient) 
   {}
 
   ngOnInit(): void {
-    
+      this.fetchTrips();
   }
+
+  fetchTrips() {
+    const params = {
+      limit: this.tripsPerPage.toString(),
+      offset:((this.currentPage - 1) * this.tripsPerPage).toString()
+    }
+
+    this.http.get<Trips[]>(`${this.url}`, { params })
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          return throwError(error);
+        })
+      )
+      .subscribe(response => {
+        this.trips = response;
+        this.totalTrips = response.length;
+        console.log(response);
+    });
+  }
+
 }
