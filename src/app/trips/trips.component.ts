502d5357e5a284c431680f4c2120c466ebc5b2dd
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-05 13:01:37 +0300

    Create shared/interfaces for trips and import to tripscomponent

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Trips } from '../../shared/interfaces'
+
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.component.html',
   styleUrls: ['./trips.component.css']
 })
 export class TripsComponent implements OnInit {
-
+  trips?: Trips[] = [];
 
   constructor (private http: HttpClient) 
   {}
---
+++ src/shared/interfaces.ts
@@ -0,0 +1,10 @@
+export interface Trips {
+    bike_departure: string;
+    bike_return: string;
+    departure_station_id: number; 
+    departure_station_name: string; 
+    return_station_id: number;
+    return_station_name: string; 
+    covered_distance_m: number; 
+    duration_sec: number;
+}
