7cc1cdc7299635891b6eb89690f1f117e86a6b71
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-10 16:04:53 +0300

    Create fetchStations function which fetch stations data to Stations[]

--- src/app/stations/stations.component.ts
+++ src/app/stations/stations.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+
+import { StationsService } from 'src/services/stations.service';
+
+import { Stations } from 'src/shared/interfaces';
 
 @Component({
   selector: 'app-stations',
   templateUrl: './stations.component.html',
   styleUrls: ['./stations.component.css']
 })
-export class StationsComponent {
+export class StationsComponent implements OnInit {
 
+  stations?: Stations[] = [];
+
+  constructor (private stationsService: StationsService) 
+  {}
+
+  ngOnInit(): void {
+    this.fetchStations();
+  }
+
+  fetchStations() {
+  this.stationsService.getStations()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        return throwError(error);
+      })
+    ).subscribe(stations => {
+      console.log(stations);
+      this.stations = stations;
+    });
+}
 }
