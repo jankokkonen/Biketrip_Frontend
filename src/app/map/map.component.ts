50b41247c424bb695532758004a9a4bd6ad071d2
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-15 17:09:09 +0300

    Fix that coordinates are correct order and now show bicyclestations on the map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,12 +12,15 @@ import { Stations } from 'src/shared/interfaces';
 export class MapComponent implements AfterViewInit {
   @ViewChild('map', { static: true }) 
   mapContainer!: ElementRef;
-  private map!: L.Map;
+  public map!: L.Map;
+  markers:L.Marker<any> [] = []
 
   constructor(private stationsService: StationsService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.addTestMarker();
+    this.addTestMarker2();
     this.getAndDisplayStations();
   }
 
@@ -30,13 +33,30 @@ export class MapComponent implements AfterViewInit {
     }).addTo(this.map);
   }
 
+  private addTestMarker(): void {
+    const marker = L.marker([60.1699, 24.9384]).addTo(this.map);
+    marker.bindPopup('Test Marker');
+  }
+
+  private addTestMarker2(): void {
+    const marker = L.marker([24.8989998259643, 60.2444085812279]).addTo(this.map);
+    marker.bindPopup('Test Marker');
+  }
+
   private getAndDisplayStations(): void {
     this.stationsService.getStations('500', '0').subscribe((stations: Stations[]) => {
       stations.forEach((station: Stations) => {
-        const marker = L.marker([station.x, station.y]).addTo(this.map);
-        // console.log(station.x)
+        const icon = L.icon({
+          iconUrl: `../../assets/img/bicycle.png`,
+          iconSize: [25, 25], // size of the icon
+          iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
+          popupAnchor: [0, -40] // point from which the popup should open relative to the iconAnchor
+        });
+  
+        const marker = L.marker([station.y, station.x],{ icon }).addTo(this.map);
         marker.bindPopup(station.name);
-      });
+      })
+      
     });
   }
   
