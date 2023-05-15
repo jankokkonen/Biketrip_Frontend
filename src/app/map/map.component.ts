25acd5566eab18c1b1c26b0ad480e714b46a9f7a
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-15 15:03:03 +0300

    Create mapcomponent getAndDisplayStations function to fetch coordinates from stationsservice

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as L from 'leaflet';
 
+import { StationsService } from 'src/services/stations.service';
+import { Stations } from 'src/shared/interfaces';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,17 +12,32 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   @ViewChild('map', { static: true }) 
   mapContainer!: ElementRef;
+  private map!: L.Map;
+
+  constructor(private stationsService: StationsService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.getAndDisplayStations();
   }
 
   private initMap(): void {
-    const map = L.map(this.mapContainer.nativeElement).setView([51.505, -0.09], 13);
-
+    this.map = L.map(this.mapContainer.nativeElement).setView([60.1699, 24.9384], 13);
+  
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 18
-    }).addTo(map);
+    }).addTo(this.map);
   }
+
+  private getAndDisplayStations(): void {
+    this.stationsService.getStations('500', '0').subscribe((stations: Stations[]) => {
+      stations.forEach((station: Stations) => {
+        const marker = L.marker([station.x, station.y]).addTo(this.map);
+        // console.log(station.x)
+        marker.bindPopup(station.name);
+      });
+    });
+  }
+  
 }
